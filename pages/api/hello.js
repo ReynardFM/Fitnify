01f9bcd8508aff9a93e5d39a5d7dc1b9e14085b7
ReynardFM <reyfitzm@gmail.com>
2025-04-16 00:10:02 -0400

    bye

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import OpenAI from 'openai';
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY,
+  defaultHeaders: {
+  },
+});
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "deepseek/deepseek-r1-distill-llama-70b:free",
+    messages: [
+      {
+        "role": "user",
+        "content": "Create an example of gym routine"
+      }
+    ],
+    
+  });
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  console.log(completion.choices[0].message);
 }
+
+main();
