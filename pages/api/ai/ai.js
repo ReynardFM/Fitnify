dc5e570170ea3e34a8a8d3e73ebfb5b0731aebd6
ReynardFM <reyfitzm@gmail.com>
2025-04-18 17:31:03 -0400

    add .env in vercel

--- pages/api/ai/ai.js
+++ pages/api/ai/ai.js
@@ -120,7 +120,7 @@ export default function AiPage({ data, plan, setPlan }) {
     const options = {
       method: 'GET',
       headers: {
-        'x-rapidapi-key': 'API_KEY here',
+        'x-rapidapi-key': project.env.EX_API,
         'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
       }
     };
@@ -153,7 +153,7 @@ export default function AiPage({ data, plan, setPlan }) {
       console.log("Start fetching fitness routine...");
       const fetchFitnessRoutine = async () => {
         try {
-          const client = new HfInference("API_KEY here");
+          const client = new HfInference(project.env.AI_API);
           
           const chatCompletion = await client.chatCompletion({
             model: "meta-llama/Llama-3.2-3B-Instruct",
